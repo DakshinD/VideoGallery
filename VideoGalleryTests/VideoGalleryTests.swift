e0a5a51567ec2c68f9e52d11590466022c95edc9
Dakshin Devanand <dakshind2005@gmail.com>
2025-02-12 11:05:45 -0600

    Initial Commit

---
+++ VideoGalleryTests/VideoGalleryTests.swift
@@ -0,0 +1,17 @@
+//
+//  VideoGalleryTests.swift
+//  VideoGalleryTests
+//
+//  Created by Dakshin Devanand on 2/12/25.
+//
+
+import Testing
+@testable import VideoGallery
+
+struct VideoGalleryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
